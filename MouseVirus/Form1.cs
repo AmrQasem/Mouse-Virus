3c7fa3a5137d171b9226b9f1f11b66fd09dff3d8
Amr Ahmed Qasem <amrkasem47@yahoo.com>
2018-05-05 13:22:05 +0200

    First Commit

---
+++ MouseVirus/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MouseVirus
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Start();
+        }
+        Point DPosition;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DPosition.X = 0;
+            DPosition.Y = 0;
+            Cursor.Position = DPosition;
+        }
+    }
+}
